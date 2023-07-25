83e1947645ca09054220510aef08033f0179ced5
Francois Ramu <francois.ramu@st.com>
2023-07-25 10:54:53 +0200

    samples: boards: stm32 flash_error detection of stm32H7x serie
    add a sample application to detect any RDSERR and/or RDPERR
    on stm32H7x platform.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

---
+++ samples/boards/stm32/flash_error/src/main.c
@@ -0,0 +1,33 @@
+#include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+#include <stm32h753xx.h>
+
+#define FLASH_IRQ_NUM     4
+
+static void flash_error_isr(void *arg) {
+    printk("ISR triggered: %u ms\n", k_uptime_get_32());
+    printk("FLASH_SR1: 0x%08x\n", FLASH->SR1);
+    printk("FLASH_SR2: 0x%08x\n", FLASH->SR2);
+
+    FLASH->CCR1 = FLASH_FLAG_ALL_BANK1;
+    FLASH->CCR2 = FLASH_FLAG_ALL_BANK2;
+}
+
+static void enable_flash_error_interrupt(void) {
+    FLASH->CR1 |= FLASH_CR_RDSERRIE;
+    FLASH->CR1 |= FLASH_CR_RDPERRIE;
+    FLASH->CR2 |= FLASH_CR_RDSERRIE;
+    FLASH->CR2 |= FLASH_CR_RDPERRIE;
+
+    IRQ_CONNECT(FLASH_IRQ_NUM, 0, flash_error_isr, NULL, 0);
+    irq_enable(FLASH_IRQ_NUM);
+}
+
+void main(void) {
+    enable_flash_error_interrupt();
+
+    while (1) {
+        // error doesn't occur if sleep is longer than 1 tick
+        k_sleep(K_TICKS(1));
+    }
+}
