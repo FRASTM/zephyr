87046c9c829d41c7825cf0a2fc742a4be58e2720
Francois Ramu <francois.ramu@st.com>
2024-01-31 17:51:09 +0100

    boards: arm: stm32 disco kit can flash on external NOR
    Change the board.cmake to add the external loader option
    to the STM32CubeProgrammer to flash the external NOR
    flash and internal memory
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- boards/arm/stm32l496g_disco/board.cmake
+++ boards/arm/stm32l496g_disco/board.cmake
@@ -2,5 +2,15 @@
 
 board_runner_args(jlink "--device=STM32L496AG" "--speed=4000")
 
+if(CONFIG_STM32_MEMMAP)
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(stm32cubeprogrammer "--hex-file=${ZEPHYR_BASE}/build/zephyr/zephyr.hex")
+board_runner_args(stm32cubeprogrammer "--extload=MX25R6435F_STM32L496G-DISCO.stldr")
+board_runner_args(stm32cubeprogrammer "--tool-opt=0x90000000")
+else()
+board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset-mode=hw" )
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
