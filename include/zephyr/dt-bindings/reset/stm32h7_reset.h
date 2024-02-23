9865730821e31f0bdf416296b5d74e460736e920
Francois Ramu <francois.ramu@st.com>
2024-02-23 09:13:58 +0100

    include: reset bindings add the stm32h7rs serie
    Add the support of the STM32H7RSX to the
    include/zephyr/dt-bindings/reset/stm32h7_reset.h
    which differs from the stm32h7 with an APB5 bus
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- include/zephyr/dt-bindings/reset/stm32h7_reset.h
+++ include/zephyr/dt-bindings/reset/stm32h7_reset.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022 Google Inc
+ * Copyright (c) 2024 STMicroelectronics
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,12 +13,18 @@
 /* RCC bus reset register offset */
 #define STM32_RESET_BUS_AHB1  0x80
 #define STM32_RESET_BUS_AHB2  0x84
+#if defined(CONFIG_SOC_SERIES_STM32H7RSX)
+#define STM32_RESET_BUS_AHB3  0xA4
+#define STM32_RESET_BUS_AHB5  0x7C
+#define STM32_RESET_BUS_APB5  0x8C
+#else
 #define STM32_RESET_BUS_AHB3  0x7C
+#define STM32_RESET_BUS_APB3  0x8C
+#endif /* CONFIG_SOC_SERIES_STM32H7RSX */
 #define STM32_RESET_BUS_AHB4  0x88
 #define STM32_RESET_BUS_APB1L 0x90
 #define STM32_RESET_BUS_APB1H 0x94
 #define STM32_RESET_BUS_APB2  0x98
-#define STM32_RESET_BUS_APB3  0x8C
 #define STM32_RESET_BUS_APB4  0x9C
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_RESET_STM32H7_RESET_H_ */
