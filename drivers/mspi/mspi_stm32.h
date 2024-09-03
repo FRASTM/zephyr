8d55d7875cb4e4bd7c14b9d1cde25d45a4047383
Francois Ramu <francois.ramu@st.com>
2024-09-03 11:32:02 +0200

    drivers: mspi: stm32 mspi functions to check controller
    Add the functions to verfify device and configure
    the mspi controller device.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/mspi/mspi_stm32.h
+++ drivers/mspi/mspi_stm32.h
@@ -26,6 +26,11 @@
 #define MSPI_TIMEOUT_US      1000000
 #define STM32_MSPI_INST_ID    0
 
+#define MSPI_MAX_FREQ        250000000
+#define MSPI_MAX_DEVICE      2
+#define MSPI_TIMEOUT_US      1000000
+#define STM32_MSPI_INST_ID    0
+
 /* Valid range is [0, 255] */
 #define MSPI_STM32_CLOCK_PRESCALER_MIN                0U
 #define MSPI_STM32_CLOCK_PRESCALER_MAX                255U
