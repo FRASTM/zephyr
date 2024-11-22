6fb6438cec691a64744b8cf0ffb7de730aa695e1
Swift Tian <swift.tian@ambiq.com>
2024-11-22 17:04:57 +0800

    samples: jesd216: add mx25 NOR flash to example
    Add the flash_mspi_nor_mx driver to the example and
    verified that flash_mspi_nor_mx_read_sfdp is working
    as expected.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- samples/drivers/jesd216/src/main.c
+++ samples/drivers/jesd216/src/main.c
@@ -16,6 +16,8 @@
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(jedec_spi_nor)
 #elif DT_HAS_COMPAT_STATUS_OKAY(jedec_mspi_nor)
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(jedec_mspi_nor)
+#elif DT_HAS_COMPAT_STATUS_OKAY(jedec_mx25)
+#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(jedec_mx25)
 #elif DT_HAS_COMPAT_STATUS_OKAY(nordic_qspi_nor)
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(nordic_qspi_nor)
 #elif DT_HAS_COMPAT_STATUS_OKAY(st_stm32_qspi_nor)
