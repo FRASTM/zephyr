38ebe6e884c1419be2d5448f3bebe53305ccd693
Francois Ramu <francois.ramu@st.com>
2024-12-19 12:11:32 +0100

    samples: basic: blinky pwm for LPTIM of the nucleo_l476 target
    Add the overlay for the nucleo_l476rg board to run the
    samples/basic/blinky_pwm/ on the PWM output of a LPTIM
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- boards/st/nucleo_l476rg/board.cmake
+++ boards/st/nucleo_l476rg/board.cmake
@@ -3,8 +3,8 @@
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32L476RG" "--speed=4000")
-
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
